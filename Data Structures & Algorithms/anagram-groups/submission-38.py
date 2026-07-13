3094e5b9e54e818f3d5f2764daf694a5208267c6
Dominic Cheang <d.cheang@hotmail.com>
2026-07-13 12:57:39 -0700

    Add: anagram-groups - submission-38

---
+++ Data Structures & Algorithms/anagram-groups/submission-38.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        gram_map = defaultdict(list)
+
+        for s in strs:
+            alpha_list = [0] * 26
+            for c in s:
+                alpha_list[ord(c) - ord('a')] += 1
+            gram_map[tuple(alpha_list)].append(s)
+        return list(gram_map.values())
