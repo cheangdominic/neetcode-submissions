e61ee923198945133bb842a266b2aed177fa6f2a
Dominic Cheang <d.cheang@hotmail.com>
2026-07-13 13:18:57 -0700

    Add: longest-consecutive-sequence - submission-32

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-32.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        nums_set = set(nums)
+
+        for n in nums_set:
+            next_num, curr_streak = n + 1, 1
+            while next_num in nums_set:
+                next_num += 1
+                curr_streak += 1
+            longest_streak = max(curr_streak, longest_streak)
+        return longest_streak
