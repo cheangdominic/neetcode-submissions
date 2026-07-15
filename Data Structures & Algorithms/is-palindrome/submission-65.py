95c6c77379b4f0b8fd53fa87304b9cffa21f9485
Dominic Cheang <d.cheang@hotmail.com>
2026-07-14 18:41:44 -0700

    Add: is-palindrome - submission-65

---
+++ Data Structures & Algorithms/is-palindrome/submission-65.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered_str = ""
+
+        for c in s:
+            if c.isalnum():
+                filtered_str += c.lower()
+        return filtered_str == filtered_str[::-1]
